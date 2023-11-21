ba7ac64823f8a6886dcb70ee697b45e2092bfdda
froilan <froilanaquino1@gmail.com>
2023-11-21 10:13:08 +0800

    init

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "forum_ui_three_aquino_froilan"
+include(":app")
